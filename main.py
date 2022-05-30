bc72a71b50c15657f12145e66b09378c63f87e67
l1menz <101088145+l1menz@users.noreply.github.com>
2022-05-30 17:15:37 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame, sys  # Import pygame
+from settings import *  # Importing from settings file
+from level import Level  # Importing from level file
+
+pygame.init()  # Initialises pygame
+WIDTH = 1600
+HEIGHT = 900
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))  # Game screen
+clock = pygame.time.Clock()  # Sets frame limit
+level = Level(level_map, SCREEN)  # Displays level
+
+while True:  # While game is running
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # When user quits
+            pygame.quit()
+            sys.exit()
+
+    SCREEN.fill('black')
+    level.run()
+
+    pygame.display.update()  # Runs what the code displays per frame
+    clock.tick(60)  # Frame the game runs at
